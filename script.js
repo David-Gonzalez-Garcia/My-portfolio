227a22ecd3b255cba69e29201035364cc77c029f
David González García <davidgonzalez@gmx.com>
2023-04-11 01:38:44 +0200

    burger menu fixed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let val;
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
     nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
+    scrollBtn.style.display = "flex";
   } else {
     nav.classList.remove("sticky");
     scrollBtn.style.display = "none";
